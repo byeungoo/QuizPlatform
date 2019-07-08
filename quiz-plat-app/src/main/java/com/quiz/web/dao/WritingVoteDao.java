95d66cb9fa5f1c0b19a57900685283796d8f3b35
dksek <dksek@DESKTOP-OU38565>
2019-07-08 13:45:41 +0900

    상세페이지 틀 구현

--- quiz-plat-app/src/main/java/com/quiz/web/dao/WritingVoteDao.java
+++ quiz-plat-app/src/main/java/com/quiz/web/dao/WritingVoteDao.java
@@ -2,12 +2,24 @@ package com.quiz.web.dao;
 
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import javax.inject.Inject;
+
+import com.quiz.web.dto.WritingVoteDto;
 
 import org.apache.ibatis.session.SqlSession;
 
 @Repository
 public class WritingVoteDao {
 
+	@Autowired
+    private SqlSession sqlSession;
+    
+    private static final String Namespace = "com.quiz.mapper.writingVoteDtlMapper";
+    
+    public List<WritingVoteDto> getWritingVoteList() throws Exception {
+        return sqlSession.selectList(Namespace+".getWritingVoteList");
+    }
+	
 }
