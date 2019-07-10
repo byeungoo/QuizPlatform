eb40353dcd927c61b24ee5b090b12a44d7285f1b
simsimjae <simsimjae@naver.com>
2019-07-10 20:03:46 +0900

    UI 버그 해결

--- quiz-plat-app/src/main/webapp/WEB-INF/views/home.jsp
+++ quiz-plat-app/src/main/webapp/WEB-INF/views/home.jsp
@@ -9,23 +9,21 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>퀴즈플랫폼</title>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.css" />
-  <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
   <link rel="stylesheet" href="resources/css/style.css">
   <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700|Roboto:400,500,700&display=swap" rel="stylesheet">
 </head>
 <body>
-  <div class="wrapper" data-role="page">
-    <header data-role="header">
-      <h3 style="height:44px;">헤더 영역</h3>
-    </header>
-    <section class="main-sec">
-      <ul class="main-sec__list" data-role="content">
-        <li class="main-sec__list-item">
-          <c:forEach items="${writingDtlDtoList}" var="writingDtlDto">
+  <div class="wrapper">
+    <div class="transition-area anim_element anim_element--slideInRight" id="main">
+      <header>
+        <h3 style="height:44px;">헤더 영역</h3>
+      </header>
+      <section class="main-sec">
+        <ul class="main-sec__list" >
+          <li class="main-sec__list-item">
+			<c:forEach items="${writingDtlDtoList}" var="writingDtlDto">
             <div class="card">
-              <a href="/detail?writing_no=${writingDtlDto.writing_no}" data-transition="slide">
+              <a href="/detail?writing_no=${writingDtlDto.writing_no}">
                 <span class="card__desc">${writingDtlDto.fir_writ_content}</span>
                 <img src="resources/img/CenterBar@2x.png" width="320px" height="25px" alt="VS" style="display:block;margin:auto;">
                 <span class="card__desc">${writingDtlDto.sec_writ_content}</span>
@@ -46,7 +44,9 @@
       </ul>
     </section>
   </div>
-  
+
+  <script src="https://code.jquery.com/jquery-2.2.4.js" ></script>  
+  <script src="resources/js/smoothState.js"></script>
   <script src="resources/js/common.js"></script>
 </body>
 </html>
--- quiz-plat-app/src/main/webapp/resources/js/common.js
+++ quiz-plat-app/src/main/webapp/resources/js/common.js
@@ -1,8 +1,82 @@
-$('.detail .card').click(function(e){
-  var footBtn = $(this).parents('.detail').find('.detail__btn');
-  var footResultBtn = footBtn.siblings('.detail__btn.on');
-  $(this).siblings('.card').removeClass('on');
-  $(this).addClass('on');
-  footBtn.hide();
-  footResultBtn.show();
-});
+(function($) {
+  // 'use strict';
+  // var $body = $('html, body'),
+  //     content = $('#main').smoothState({
+  //       debug : true,
+  //       onStart: {
+  //         duration: 0,
+  //         render: function (url,$container) {
+  //           $("html, body").animate({ scrollTop: "0px" });
+  //           //make a duplicate container for animation...
+  //           var $newContainer = $container.clone();
+  //           $newContainer.attr("id", "main");
+  //           $newContainer.css({position:'absolute', top:$container.offset().top, width:$container.css("width")});
+  //           $newContainer.css({height:$container.css("height")});
+  //           //$container.css({height:$container.css("height")});
+  //           $container.empty(); //empty the old content so that it takes up 0 space
+  //           $container.before($newContainer); //and immediately add the duplicate back on
+  //           $('.transition-area').get(1).remove();
+  //           var element =  $('.anim_element');
+  //           element.removeClass('anim_element--slideInRight'); // just in case we have the class still
+  //           setTimeout(callAnimation(element, true), 0); //start the animation
+  //         }
+  //       },
+  //       onReady: {
+  //         duration: 0,
+  //         render: function ($container, $newContent) {
+  //           // Inject the new content
+  //           debugger;
+  //           $container.html($newContent);
+        
+  //           // do animations
+  //           var element = document.getElementById($container[0].id).getElementsByClassName('row')[0];
+        
+  //           callAnimation(element);
+  //         }
+  //       },
+  //       // Runs when a link has been activated
+  //       onAfter: {
+  //         duration: 250, // Duration of our animation
+  //         render: function (url, $container) {
+  //           // toggleAnimationClass() is a public method
+  //           // for restarting css animations with a class
+  //           content.toggleAnimationClass('is-exiting');
+  //           // Scroll user to the top
+  //           $body.animate({scrollTop: 0});
+  //         }
+  //       }
+  //     }).data('smoothState');
+      
+      // function callAnimation(element, exiting) {
+      //   if (!exiting) {
+      //       $(element).addClass("anim_element--slideInRight");
+      //   } else {
+      //       $(element).addClass('anim_element--slideInRight');
+      //   }
+      // }
+      $('.card--single').click(function(){
+        $('.detail__btn').addClass('on');
+      });
+      
+      $('.write__inpcnt, .write__txtarea').on('keyup',function(){
+        var len = $(this).val().trim().length;
+        if(len > 0) $(this).addClass('on');
+        else if(len == 0) $(this).removeClass('on');
+        if(isAllChecked.call($(this))){
+          $('.write__footbtn').hide();
+          $('.write__footbtn.on').show();
+        }else{
+          $('.write__footbtn').show();
+          $('.write__footbtn.on').hide();
+        }
+      });
+
+      function isAllChecked(){
+        var chk = 0;
+        $('.write__inpcnt, .write__txtarea').each(function(index,item){
+          $(item).hasClass('on') ? chk++ : chk;
+        });
+        return chk == 3 ? true : false;
+      }
+      
+})(jQuery);
