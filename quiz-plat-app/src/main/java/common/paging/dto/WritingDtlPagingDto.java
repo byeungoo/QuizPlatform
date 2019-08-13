b3c4ebb28af901b1879d001190a5a834db74b8ce
simsimjae <simsimjae@naver.com>
2019-08-13 22:20:44 +0900

    Revert "Revert "Merge branch 'master' of https://github.com/dksekfldks65/QuizPlatform""
    This reverts commit 526be4daa6987bd75ae8e1449b5c065ad0e183c6.

---
+++ quiz-plat-app/src/main/java/common/paging/dto/WritingDtlPagingDto.java
@@ -0,0 +1,15 @@
+package common.paging.dto;
+
+public class WritingDtlPagingDto extends PagingDto{
+
+	int writing_no;
+
+	public int getWriting_no() {
+		return writing_no;
+	}
+
+	public void setWriting_no(int writing_no) {
+		this.writing_no = writing_no;
+	}
+	
+}
