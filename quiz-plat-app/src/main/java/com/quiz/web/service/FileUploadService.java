4956b2c71055b5c608ddcfa3cbe35a93ad1f6151
ByeonGooHoon <dksekfldks65@naver.com>
2019-10-03 19:57:47 +0900

    파일 절대경로 로직 삭제. 하드코딩 url 넣음

--- quiz-plat-app/src/main/java/com/quiz/web/service/FileUploadService.java
+++ quiz-plat-app/src/main/java/com/quiz/web/service/FileUploadService.java
@@ -4,21 +4,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
-import javax.servlet.ServletContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
-	
-	@Autowired private ServletContext servletContext;
-	
 	// 리눅스 기준으로 파일 경로를 작성 ( 루트 경로인 /으로 시작한다. )
 	// 윈도우라면 workspace의 드라이브를 파악하여 JVM이 알아서 처리해준다.
 	// 따라서 workspace가 C드라이브에 있다면 C드라이브에 upload 폴더를 생성해 놓아야 한다.
-	private static final String SAVE_PATH = "/upload";
+	private static final String SAVE_PATH = "/home/ubuntu/tomcat8/webapps/upload";
 	private static final String PREFIX_URL = "/upload/";
 	
 	public String restore(MultipartFile multipartFile) {
@@ -76,8 +70,7 @@ public class FileUploadService {
 		boolean result = false;
 
 		byte[] data = multipartFile.getBytes();
-		String realPath = servletContext.getRealPath("/");
-		FileOutputStream fos = new FileOutputStream(realPath + SAVE_PATH + "/" + saveFileName);
+		FileOutputStream fos = new FileOutputStream(SAVE_PATH + "/" + saveFileName);
 		fos.write(data);
 		fos.close();
 		
