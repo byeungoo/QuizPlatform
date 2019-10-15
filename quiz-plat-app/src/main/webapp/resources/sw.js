e3d4160e7e5313cb080396f7350f2c109bfa96f2
simsimjae <simsimjae@naver.com>
2019-10-16 03:49:59 +0900

    [UI] 상세 작업

--- quiz-plat-app/src/main/webapp/resources/sw.js
+++ quiz-plat-app/src/main/webapp/resources/sw.js
@@ -1,4 +1,4 @@
-importScripts("/DevPickVs/resources/precache-manifest.8c15c68311b61c2a1b947ec825397a86.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/DevPickVs/resources/precache-manifest.5988785334ce56969d150eaf627fc9b6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
