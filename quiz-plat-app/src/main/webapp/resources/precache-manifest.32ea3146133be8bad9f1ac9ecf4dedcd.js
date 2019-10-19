41fad3fa0dd4bf09ac687671eb606f050a542969
simsimjae <simsimjae@naver.com>
2019-10-19 23:58:52 +0900

    경로변경

---
+++ quiz-plat-app/src/main/webapp/resources/precache-manifest.32ea3146133be8bad9f1ac9ecf4dedcd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe40413e5f06fc9dd79",
+    "url": "/resources/app.bundle.js"
+  },
+  {
+    "revision": "abe40413e5f06fc9dd79",
+    "url": "/resources/app.css"
+  },
+  {
+    "revision": "38230fecd04026181cc7d8b121409eda",
+    "url": "/resources/img/hot@2x.png"
+  },
+  {
+    "revision": "44ae4fdb16ba192549377318d3e93784",
+    "url": "/resources/img/jquery.minicolors.png"
+  },
+  {
+    "revision": "eb0bf4fad6beb099f5391ebd6919357a",
+    "url": "/resources/img/new@2x.png"
+  },
+  {
+    "revision": "f7e7ba8013e1ffeeed0b63070e863905",
+    "url": "/resources/img/question@2x.png"
+  },
+  {
+    "revision": "cde4be54afa344c007cf",
+    "url": "/resources/share.bundle.js"
+  },
+  {
+    "revision": "09f8a6af75ef47a0dd82",
+    "url": "/resources/vendors.bundle.js"
+  },
+  {
+    "revision": "afc7fdcec02106dc8993",
+    "url": "/resources/write.bundle.js"
+  },
+  {
+    "revision": "afc7fdcec02106dc8993",
+    "url": "/resources/write.css"
+  }
+]);
