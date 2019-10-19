5b657c630465547a326692b5fe66d5133b257b87
simsimjae <simsimjae@naver.com>
2019-10-19 22:36:26 +0900

    [UI] write페이지 경로 변경 및 카카오 공유하기 프론트 웹팩 경로 타게 변경

---
+++ quiz-plat-app/src/main/webapp/resources/resources/precache-manifest.db6436c212f105365b804f8b3c176d2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3594ce4d1eb97d9481",
+    "url": "/resources/app.bundle.js"
+  },
+  {
+    "revision": "ec3594ce4d1eb97d9481",
+    "url": "/resources/app.css"
+  },
+  {
+    "revision": "38230fecd04026181cc7d8b121409eda",
+    "url": "/resources/img/hot@2x.png"
+  },
+  {
+    "revision": "eb0bf4fad6beb099f5391ebd6919357a",
+    "url": "/resources/img/new@2x.png"
+  },
+  {
+    "revision": "f7e7ba8013e1ffeeed0b63070e863905",
+    "url": "/resources/img/question@2x.png"
+  },
+  {
+    "revision": "cde4be54afa344c007cf",
+    "url": "/resources/share.bundle.js"
+  },
+  {
+    "revision": "574ecfea6c03ddc6e218",
+    "url": "/resources/vendors.bundle.js"
+  },
+  {
+    "revision": "8294e187d1ff5311325b",
+    "url": "/resources/write.bundle.js"
+  }
+]);
