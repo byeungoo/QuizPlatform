0c0b381f6fa60cc7c9baca78b4c07c6415991663
simsimjae <simsimjae@naver.com>
2019-10-31 20:32:30 +0900

    cors

--- quiz-plat-app/src/main/java/common/SimpleCORSFilter.java
+++ quiz-plat-app/src/main/java/common/SimpleCORSFilter.java
@@ -27,7 +27,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         response.setHeader("Access-Control-Allow-Credentials", true);
         
-        response.setHeader("Access-Control-Allow-Origin", "https://172.20.10.2");
+        response.setHeader("Access-Control-Allow-Origin", "https://localhost");
      
  
  
