3e230239ac64ff118959a3215757ce3cb4cc2954
simsimjae <simsimjae@naver.com>
2019-10-31 20:23:56 +0900

    CORS설정 변경

--- quiz-plat-app/src/main/java/common/SimpleCORSFilter.java
+++ quiz-plat-app/src/main/java/common/SimpleCORSFilter.java
@@ -25,8 +25,9 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Credentials", true);
         
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "https://172.20.10.2");
      
  
  
